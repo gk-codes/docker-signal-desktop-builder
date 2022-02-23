86f4dcb14370332f95dd90948a168d9dd0d3047f
GK <5641550+gk-codes@users.noreply.github.com>
2022-02-23 21:16:45 +0100

    add comments, replace curl with wget

--- build.sh
+++ build.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
+echo "build.sh"
+
 echo "Retrieving latest Signal version..."
 wget --quiet https://api.github.com/repos/signalapp/Signal-Desktop/releases -O releases.json
-# Get the version from the tag_name and the download URL.
+# Get version from tag_name and download URL
 signalversion=$(jq . releases.json | grep tag_name | grep -v beta | head -n 1 | cut -d'"' -f4 | tr -d 'v')
 echo ${signalversion} > signalversion.txt
 echo "Latest Signal version: ${signalversion}"
 
 echo "Retrieving Node.js version..."
-curl -Ls -o nodejsversion.txt https://raw.githubusercontent.com/signalapp/Signal-Desktop/v${signalversion}/.nvmrc
+wget --quiet https://raw.githubusercontent.com/signalapp/Signal-Desktop/v${signalversion}/.nvmrc -O nodejsversion.txt
 nodejsversion=$(cat nodejsversion.txt)
 echo "Latest Node.js version: ${nodejsversion}"
 
+echo "Building container..."
 podman build --format docker \
     --build-arg NODE_VERSION=${nodejsversion} \
     --build-arg SIGNAL_VERSION=${signalversion} \
     -t build-signal-desktop .
 
+echo "Done"
 exit 0
